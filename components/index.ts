e44f4eea7e381fcf6a0de3d7af4fb717bdb77ca6
Andreas Bigger <abigger87@gmail.com>
2021-03-06 21:08:11 -0800

    fix publish

--- components/index.ts
+++ components/index.ts
@@ -2,5 +2,5 @@ export { default as Footer } from "./Footer";
 export { default as Head } from "./Head";
 export { default as Header } from "./Header";
 export { default as Provider } from "./Provider";
-export { default as QueryList } from "./QueryList";
+export { default as ContractList } from "./ContractList";
 export { default as Roadrunner } from "./Roadrunner";
